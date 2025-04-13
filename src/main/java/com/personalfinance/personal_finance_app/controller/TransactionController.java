8318386b04151a6362fa7bc927c0a43b46eb1d45
Anton20HD <anton.haglund@iths.se>
2025-04-13 11:03:29 +0200

    CRUD operations fully integrated for transaction. Add update and delete functions

--- src/main/java/com/personalfinance/personal_finance_app/controller/TransactionController.java
+++ src/main/java/com/personalfinance/personal_finance_app/controller/TransactionController.java
@@ -3,9 +3,11 @@ package com.personalfinance.personal_finance_app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +28,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class TransactionController {
 
-
-    //Uses final for only assigning a variable once, cant be changed
+    // Uses final for only assigning a variable once, cant be changed
     private final TransactionService transactionService;
 
     @GetMapping
@@ -44,6 +45,17 @@ public class TransactionController {
     public List<Transaction> getOneCategory(@PathVariable String category) {
         return transactionService.getTransactionByCategory(category);
     }
+
+    @PutMapping("/{id}")
+    public Transaction updateTransaction(@RequestBody Transaction transaction, @PathVariable String id) {
+            return transactionService.updateTransaction(transaction, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Transaction deleteTransaction(@PathVariable String id) {
+                return transactionService.deleteTransaction(id);
+    }
+    
 
 
 
--- src/main/java/com/personalfinance/personal_finance_app/service/TransactionService.java
+++ src/main/java/com/personalfinance/personal_finance_app/service/TransactionService.java
@@ -1,8 +1,13 @@
 package com.personalfinance.personal_finance_app.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Locale.Category;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.personalfinance.personal_finance_app.model.Transaction;
 import com.personalfinance.personal_finance_app.repository.TransactionRepo;
@@ -12,11 +17,11 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class TransactionService {
-    
+
     private final TransactionRepo transactionRepo;
 
     public List<Transaction> getAllTransactions() {
-            return transactionRepo.findAll();
+        return transactionRepo.findAll();
 
     }
 
@@ -28,9 +33,25 @@ public class TransactionService {
         return transactionRepo.findByCategory(category);
     }
 
-    
+    public Transaction updateTransaction(Transaction transaction, String id) {
+        Transaction updateTransaction = transactionRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Transaction not found with id:" + id));
 
+        updateTransaction.setTitle(transaction.getTitle());
+        updateTransaction.setAmount(transaction.getAmount());
+        updateTransaction.setDate(transaction.getDate());
+        updateTransaction.setCategory(transaction.getCategory());
 
+        return transactionRepo.save(updateTransaction);
 
+    }
 
+    public Transaction deleteTransaction(String id) {
+        Transaction transaction = transactionRepo.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Transaction with id" + id + "not found"));
+
+        transactionRepo.delete(transaction);
+        return transaction;
+
+    }
 }
