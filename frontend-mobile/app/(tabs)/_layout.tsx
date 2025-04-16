56bc1728f5b7dda52390cbaa4fd4efde68024d77
Anton20HD <anton.haglund@iths.se>
2025-04-16 12:43:38 +0200

    Add a colors.ts inside a constants folder to apply colors directly to icons. Add icons to the navbar with an active and a disabled tab color

--- frontend-mobile/app/(tabs)/_layout.tsx
+++ frontend-mobile/app/(tabs)/_layout.tsx
@@ -1,6 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { Entypo } from '@expo/vector-icons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import {colors} from "../../constants/colors";
 
 const _Layout = () => {
   return (
@@ -8,19 +11,43 @@ const _Layout = () => {
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: "Home", headerShown: false }}
+        options={{ title: "Home", headerShown: false,
+            tabBarIcon: ({ focused }) => (
+                <>
+                <Entypo name="home" color={focused ? colors.active : colors.disabled} size={24} />
+                </>
+            )
+         }}
       ></Tabs.Screen>
       <Tabs.Screen
         name="Statistics"
-        options={{ title: "Statistics", headerShown: false }}
+        options={{ title: "Statistics", headerShown: false,
+            tabBarIcon: ({ focused }) => (
+                <>
+                <Entypo name="bar-graph" color={focused ? colors.active : colors.disabled} size={24} />
+                </>
+            )
+         }}
       ></Tabs.Screen>
       <Tabs.Screen
         name="TotalBalance"
-        options={{ title: "Wallet", headerShown: false }}
+        options={{ title: "Wallet", headerShown: false,
+            tabBarIcon: ({ focused }) => (
+                <>
+                <Entypo name="wallet" color={focused ? colors.active : colors.disabled} size={24} />
+                </>
+            )
+         }}
       ></Tabs.Screen>
       <Tabs.Screen
         name="Profile"
-        options={{ title: "Profile", headerShown: false }}
+        options={{ title: "Profile", headerShown: false,
+            tabBarIcon: ({ focused }) => (
+                <>
+                <MaterialCommunityIcons name="account" color={focused ? colors.active : colors.disabled} size={24} />
+                </>
+            )
+         }}
       ></Tabs.Screen>
     </Tabs>
   );
---
+++ frontend-mobile/constants/colors.ts
@@ -0,0 +1,13 @@
+ export const colors = {
+    primary: '#0B1525',
+    secondary: '#3A86FF',
+    active: '#3A86FF',
+    disabled: '#A0AEC0',
+    positive: '#10B981',
+    negative: '#B91010',
+    primaryText: '#FFFFFF',
+    secondaryText: '#A0AEC0',
+  };
+
+ 
+  
