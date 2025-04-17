4f74fb56c336b8595bacf1f9422cfe537f773e75
Anton20HD <anton.haglund@iths.se>
2025-04-17 20:12:29 +0200

    Add web config file to fix CORS error

---
+++ src/main/java/com/personalfinance/personal_finance_app/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.personalfinance.personal_finance_app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOrigins(
+            "http://localhost:8081",
+            "https://personal-finance-app-production-693d.up.railway.app"
+        
+        )
+        .allowedMethods("GET", "POST", "PUT", "DELETE")  
+        .allowedHeaders("*");  
+    }
+
+
+}
