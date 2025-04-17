7831629d3f2b944540b264c63f3640f9424cb987
Anton20HD <anton.haglund@iths.se>
2025-04-17 10:23:36 +0200

    Customize the backgroundcolor for the tab bar and some positioning. Import different icons from expo

--- frontend-mobile/app/(tabs)/_layout.tsx
+++ frontend-mobile/app/(tabs)/_layout.tsx
@@ -1,53 +1,98 @@
 import { View, Text, ImageBackground } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
-import { Entypo } from '@expo/vector-icons';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import {colors} from "../../constants/colors";
+import { Entypo } from "@expo/vector-icons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { colors } from "../../constants/colors";
+
+// const TabIcon = () => {
+//         return(
+
+//         )
+
+// }
 
 const _Layout = () => {
   return (
     //Gömmer den specifika sidan som du är inne på
-    <Tabs>
+    <Tabs screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: '100%',
+                height: '100%',
+                justifyContent: 'center',
+                alignItems: 'center'
+            },
+            tabBarStyle: {
+                backgroundColor: '#0B1525',
+                height: 60,
+                padding:10,
+            }
+
+    }}>
       <Tabs.Screen
         name="index"
-        options={{ title: "Home", headerShown: false,
-            tabBarIcon: ({ focused }) => (
-                <>
-                <Entypo name="home" color={focused ? colors.active : colors.disabled} size={24} />
-                </>
-            )
-         }}
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <Entypo
+                name="home"
+                color={focused ? colors.active : colors.disabled}
+                size={30}
+              />
+            </>
+          ),
+        }}
       ></Tabs.Screen>
       <Tabs.Screen
         name="Statistics"
-        options={{ title: "Statistics", headerShown: false,
-            tabBarIcon: ({ focused }) => (
-                <>
-                <Entypo name="bar-graph" color={focused ? colors.active : colors.disabled} size={24} />
-                </>
-            )
-         }}
+        options={{
+          title: "Statistics",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <Entypo
+                name="bar-graph"
+                color={focused ? colors.active : colors.disabled}
+                size={30}
+              />
+            </>
+          ),
+        }}
       ></Tabs.Screen>
       <Tabs.Screen
         name="TotalBalance"
-        options={{ title: "Wallet", headerShown: false,
-            tabBarIcon: ({ focused }) => (
-                <>
-                <Entypo name="wallet" color={focused ? colors.active : colors.disabled} size={24} />
-                </>
-            )
-         }}
+        options={{
+          title: "Wallet",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <Entypo
+                name="wallet"
+                color={focused ? colors.active : colors.disabled}
+                size={30}
+              />
+            </>
+          ),
+        }}
       ></Tabs.Screen>
       <Tabs.Screen
         name="Profile"
-        options={{ title: "Profile", headerShown: false,
-            tabBarIcon: ({ focused }) => (
-                <>
-                <MaterialCommunityIcons name="account" color={focused ? colors.active : colors.disabled} size={24} />
-                </>
-            )
-         }}
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <MaterialCommunityIcons
+                name="account"
+                color={focused ? colors.active : colors.disabled}
+                size={30}
+              />
+            </>
+          ),
+        }}
       ></Tabs.Screen>
     </Tabs>
   );
