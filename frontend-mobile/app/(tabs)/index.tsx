2ec356f1f7b9ad6637c7f149050ecb1bb5ff4013
Anton20HD <anton.haglund@iths.se>
2025-04-17 19:36:31 +0200

    Ignore components folder for now as it give errors. Add temporary hardcoded amount for the wallet. Add different fonts

--- frontend-mobile/app/(tabs)/index.tsx
+++ frontend-mobile/app/(tabs)/index.tsx
@@ -15,13 +15,57 @@ export default function Index() {
         className="flex-1"
         imageStyle={{ height: "100%", width: "100%" }}
       >
-        <ScrollView className="flex-1 flex-col px-5">
-       <View>
-        <GreetingTextAndSearchIcon/>
-       </View>
-        <View>
-          <TotalBalanceBorderBox/>
-        </View>
+        <ScrollView className="flex-1 flex-col px-5 mt-10">
+          <View className="flex-1 flex-row-reverse justify-between content-center">
+            <Button
+              className="w-12 ml-auto mt-10 flex-1 p-1 h-8 "
+              radius={"50"}
+              type="solid"
+              color={colors.secondary}
+            >
+              <SearchIcon name="search" size={18} color={colors.primaryText} />
+            </Button>
+            <Text
+              style={{ fontFamily: "Inter", fontSize: 30, fontWeight: "bold" }}
+              className="mt-10 mb-5 text-primaryText"
+            >
+              <Text
+                style={{ fontFamily: "Inter", fontSize: 20, fontWeight: "300" }}
+                className="text-secondaryText"
+              >
+                Hello,
+              </Text>
+              {"\n"}User
+            </Text>
+          </View>
+          <View className="mx-0 my-6 p-5 bg-white rounded-xl shadow-lg border border-gray-100 h-52">
+            <View className="flex-row justify-between items-center mb-4">
+              <Text 
+                className="mb-1"
+                style={{ fontFamily: "Inter", fontSize: 22, fontWeight: "500" }}
+              >
+                Total Balance:{" "}
+                <Text style={{ fontFamily: "Inter", fontSize: 30 }} className="font-bold ">
+                  {"\n"}$400.00
+                </Text>
+              </Text>
+            </View>
+
+            <View className="h-px bg-gray-100 my-3" />
+
+            <View className="flex-row justify-between mt-2">
+              <View className="items-start">
+                <Text  style={{ fontFamily: "Inter", fontSize: 20, fontWeight: "500" }}>Income:<Text className="font-semibold text-positive">{"\n"}$800.00</Text></Text>
+              </View>
+              <View className="items-end">
+                <Text  style={{ fontFamily: "Inter", fontSize: 20, fontWeight: "500" }}>Expense:<Text className="font-semibold text-negative">{"\n"}$300.00</Text></Text>
+              </View>
+            </View>
+          </View>
+          <View className="mt-10">
+          <Text style={{ fontFamily: "Inter", fontSize: 25, fontWeight: "300" }} className="text-primaryText font-ligh">Recent Transactions:</Text>
+
+          </View>
         </ScrollView>
       </ImageBackground>
     </View>
